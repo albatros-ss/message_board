b27dd7d786e7e310dce2433020533aaf56c5eaa0
Vadim Volodko <albatros.andr@gmail.com>
2019-03-24 17:33:46 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-tile v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Ad applications</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="link of links" :key="link.title" :to="link.url" flat>
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "lock", url: "/login" },
+        { title: "Registration", icon: "face", url: "/registration" },
+        { title: "User", icon: "bookmark_border", url: "/orders" },
+        { title: "New ad", icon: "note_add", url: "/new" },
+        { title: "My ads", icon: "list", url: "/list" }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  productionSourceMap: false,
+
+  css: {
+    sourceMap: process.env.NODE_ENV !== "production",
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "./src/assets/stylesheets/_variables.scss";
+          @import "./src/assets/stylesheets/_mixins.scss";
+        `
+      }
+    }
+  },
+
+  lintOnSave: undefined
+};
